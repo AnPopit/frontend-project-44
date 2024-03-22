f84247c1c0618dfd2394786f74d3ce09a9ea6bf8
Politova <anna_loginovskikh@mail.ru>
2024-03-22 15:29:10 +0500

    stage 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,5 +3,3 @@ import NameUser from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${NameUser()}!`);
-
-
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const NameUser = () => {
-    const Name = readlineSync.question('May I have your name? ');
-    return Name;
-  };
-  
-  export default NameUser;
+  const Name = readlineSync.question('May I have your name? ');
+  return Name;
+};
+
+export default NameUser;
